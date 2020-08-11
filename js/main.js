3bcf5dd91012c00790dd1f5497e7917641ac74a7
Michael Blix <m.blix@samsung.com>
2020-08-11 01:08:52 +0000

    Add setModel function; sample

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const modelViewer = document.querySelector('model-viewer');
+
+let samples = ['https://modelviewer.dev/shared-assets/models/Astronaut.glb'];
+
 function load() {
   let params = new URLSearchParams(location.search);
 
@@ -11,7 +15,19 @@ function load() {
   if (params.has('url')) {
     url = params.get('url');
     console.log(`has 'url' param`);
+
+    setModel(url);
   }
+
+  let button = document.getElementById('sample-btn');
+  button.addEventListener('click', function(e){
+    setModel(samples[0]);
+  });
+}
+
+function setModel(url) {
+  console.log('setModel: ' + url);
+  modelViewer.src = url;
 }
 
 window.onload = load;
